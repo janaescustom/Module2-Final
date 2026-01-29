f944cfce3933690db8d349048ef3f030aa2fc16f
janaescustom <janaescustom@gmail.com>
2026-01-29 15:21:31 +0100

    values list @media max-width:768px

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('javascript')
+
+
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open') 
+}
